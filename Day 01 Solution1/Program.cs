8ffb76c884868cf78e2939ce0aa9b8e816c7c290
mohammedtark <zazazaaaa468s@gmail.com>
2024-06-29 19:38:03 +0300

    first commit

---
+++ Day 01 Solution1/Program.cs
@@ -0,0 +1,198 @@
+﻿using System.Data.SqlTypes;
+
+namespace Day_01_Solution1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Assignment 2
+
+            #region  Q1 
+            //Write a program that allows the user to enter a number then print it.
+
+            //int Number;
+            //Console.WriteLine("Enter a Number");
+            //Number = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Your Nomber Is {Number}");
+            #endregion
+
+            #region Q2
+            //Write C# program that converts a string to an integer, but the string contains non-numeric characters. And mention what will happen
+
+            //string number = "123abc";
+            //int result = Convert.ToInt32(number);
+            #endregion
+
+            #region Q3 
+            //Write C# program that Perform a simple arithmetic operation with floating-point numbers And mention what will happen
+
+
+            //double num1 = 0.1;
+            //double num2 = 0.2;
+            //double sum = num1 + num2;
+
+            //Console.WriteLine($"The Result = {sum}");
+
+            #endregion
+
+            #region Q4 
+            //Write C# program that Extract a substring from a given string.
+
+            //string input = "Hello, World!";
+            //string subString = input.Substring(7, 5);
+            //Console.WriteLine($"The SubString = {subString}");
+
+            #endregion
+
+            #region Q5
+            //Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
+
+            //int X = 1;
+            //int Y = 2;
+
+            //Console.WriteLine($"X = {X} , Y = {Y}");
+            //Console.WriteLine("-------------------------");
+            //Y = X;
+            //Y = 5;
+
+            //Console.WriteLine($"X = {X} , Y = {Y}");
+
+
+            #endregion
+
+            #region Q6 
+            // Write C# program that Assigning one reference type variable to another and modifying the object through one variable and mention what will happen
+
+
+            //int[] arr1 = { 1, 2, 3, 4 };
+            //int[] arr2 = arr1;
+            //arr2[1] = 100;
+
+            //Console.WriteLine(arr1[1]);
+            //Console.WriteLine(arr2[1]);
+            #endregion
+
+            #region Q7 
+            //Write C# program that take two string variables and print them as one variable
+
+            //string Word1 = "Hello, ";
+            //string Word2 = "World!";
+
+            //Console.WriteLine($"{Word1} {Word2}");
+
+            #endregion
+
+            #region Q8
+            // Write a program that calculates the simple interest given the principal amount, rate of interest, and time. The formula for simple interest is Interest = (principal * rate * time) / 100.
+
+            //double principal = 1000.0;
+            //double rate = 6.0;
+            //double time = 3.0;
+            //double Interest = (principal * rate * time) / 100;
+
+            //Console.WriteLine($"Simple Interest: {Interest}");
+            #endregion
+
+            #region Q9
+            // Write a program that calculates the Body Mass Index(BMI) given a person's weight in kilograms and height in meters. The formula for BMI is BMI = (Weight) / (Height * Height)
+
+            //double Weight = 80.0;
+            //double Height = 1.80;
+            //double BMI = Weight / (Height * Height);
+
+            //Console.WriteLine($"BMI: {BMI}");
+            #endregion
+
+            #region Q10
+            // Write a program that uses the ternary operator to check if the temperature is too hot, too cold, or just good. Assign the result in variable then display the result.Assume that below 10 degrees is "Too Cold", above 30 degrees is "Just Hot", and anything else is "Just Good".
+
+            //double Temperature = 25.0;
+            //string TempCheck = Temperature < 10 ? "Too Cold" : (Temperature > 30 ? "Just Hot" : "Just Good");
+            //Console.WriteLine($"Temperature IS: {TempCheck}");
+            #endregion
+
+            #region Q11
+            //11 - Write a program that takes the date from user and displays it in various formats using string interpolation.
+            //Ex:
+            //Today’s date : 20 , 11 , 2001
+            //Today's date : 20 / 11 / 2001
+            //Today's date : 20 – 11 – 2001
+            //-------------------------------
+
+            //DateTime CurrentDate = DateTime.Now;
+            //string FormattedDate01 = $"Today’s date : {CurrentDate.Day},{CurrentDate.Month},{CurrentDate.Year}";
+            //string FormattedDate02 = $"Today’s date : {CurrentDate.Day}/{CurrentDate.Month}/{CurrentDate.Year}";
+            //string FormattedDate03 = $"Today’s date : {CurrentDate.Day}-{CurrentDate.Month}-{CurrentDate.Year}";
+
+
+            //Console.WriteLine(FormattedDate01);
+            //Console.WriteLine(FormattedDate02);
+            //Console.WriteLine(FormattedDate03);
+            #endregion
+
+            #region Q12
+            //What is the output of the following C# code?
+
+            //DateTime date = new DateTime(2024, 6, 14);
+            //Console.WriteLine($"The event is on {date:MM/dd/yyyy}");
+
+            ////a) The event is on 14/06/2024
+            ////b) The event is on 2024-06-14
+            ////c) The event is on 06/14/2024
+            ////d) The event is on June 14, 2024
+
+            //// The correct statement is:
+            //// Ansewr --> C
+            //// c  The event is on 06/14/2024
+            #endregion
+
+            #region Q13
+            //Which of the following statements is correct about the C#.NET code snippet given below?
+
+            //int d;
+            //d = Convert.ToInt32(!(30 < 20));
+            //Console.WriteLine(d);
+
+
+            //// The correct statement is:
+            ///// Ansewr --> f
+            //// A value 1 will be assigned to d.
+            //// This is because !(30 < 20) evaluates to true, and Convert.ToInt32(true) returns 1.
+
+            #endregion
+
+            #region Q14
+            //Which of the following is the correct output for the C# code given below?
+
+            //Console.WriteLine(13 / 2 + " " + 13 % 2);
+
+
+            //// The correct output is:
+            ///// Answer --> d
+            //// 6 1
+            //// 13 / 2 = 6 and 13 % 2 = 1
+
+            #endregion
+
+            #region Q15
+            //What will be the output of the C# code given below?
+
+            //int num = 1, z = 5;
+            //if (!(num <= 0))
+            //    Console.WriteLine(++num + z++ + " " + ++z);
+            //else
+            //    Console.WriteLine(--num + z-- + " " + --z);
+
+
+            //// The correct output is:
+            ///// Answer --> D
+            //// 7 7
+            //// ++num is 2, z++ is 5, making it 7, and ++z is 7.
+
+            #endregion
+
+            #endregion
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,246 @@
+﻿
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variable Declaration
+            //int Number;
+            //Number = 10;
+            //Console.WriteLine(Number);
+
+            //Variable : 
+            //DataType: int
+            //name : number
+            //size : 4 Bytes
+            //Value : 10
+            //Address: Hexa-Decimal
+
+            //int Number = 10;
+            //int Number1 = 10, Number2 = 11, Number3 = 12; 
+            //int Age = 19;
+            //int class; //Invalid
+            // int int
+            //int namespace
+
+            //Name Convention:
+            //Pascal Case --> FirstName, C#
+            //Camel Case  --> firstName
+            //Kabab Case --> first_name
+
+
+            #endregion
+
+            #region Datatype - value Type (Primitive)
+            //Datatype - value Type (primitive)
+
+            //int Number;
+            //Console.WriteLine(Number);
+            //Int32 Number1 = 12;
+            //Number = Number1;
+            //Console.WriteLine(Number);
+            //Console.WriteLine(Number1);
+            #endregion
+
+            #region Datatype - Reference Type (NonPrimitive)
+            //Point P1;
+            //Declare 
+            //P1 = new Point();
+            //Point P2 = new Point();
+            //P2 = P1;
+            //P2.x = 6;
+            //Console.WriteLine(P1.x);//6
+            //Console.WriteLine(P2.x);//6
+            //Console.WriteLine(P2.y);//0
+            #endregion
+
+            #region Object
+            //Point p1 = new Point();
+            //p1.ToString();
+            //p1.GetHashCode();
+            //p1.Equals();
+            //p1.GetType();
+
+
+            //object O1;
+            //O1 = new object();
+            //O1 = new Point();
+            //O1 = new string("Ahmed");
+            //O1 = 1 ;
+            //O1 = "Ahmed";
+            //O1 = true ;
+            //O1 = 3.14;
+
+            //object O1 = new object();
+            //object O2 = new object();
+
+            //O2 = O1;
+
+            //Console.WriteLine(O1.GetHashCode());//54267293
+            //Console.WriteLine(O2.GetHashCode());//18643596 
+            #endregion
+
+            #region Implicit Casting Explicit Casting
+            //int x = 4;
+            //double y = x;
+
+            //int x =5;
+            //long y = x;
+
+            //2.Explicit
+
+            //int x = 5;
+            //long Y = 22626262262226262626626262;
+            //x = (int) Y;
+            //Console.WriteLine(x); // -1327026069
+
+
+            //int x = 5;  // 4 Bytes
+            //long Y;  // 8 Bytes
+            //checked
+            //{
+            //    unchecked
+            //    {
+            //        Y = 22626262262226262626626262;
+            //    }
+            //    x = (int)Y;
+            //}
+            //Console.WriteLine(x);// -1327026069 
+            #endregion
+
+            #region Parse & TryParse Function And Convert Class
+            //string Name = Console.ReadLine();
+            //Console.WriteLine("Hello" + Name);
+
+            //int x= int.Parse("123");
+            //Console.WriteLine(x+5);
+
+            //Console.Write("Enter Your Name: ");
+            //string Name = Console.ReadLine();
+
+            //Console.WriteLine("Enter Your Age: ");
+
+            //int Age = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Name : " + Name);
+            //Console.WriteLine("Age : " + Age);
+
+            //Console.Write("Enter Your GPA");
+            //double GPA = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("----------------------------------------");
+            //Console.WriteLine("Name : " + Name);
+            //Console.WriteLine("Age : " + Age);
+            //Console.WriteLine("GPA : " + GPA);
+
+            //Convert
+            //Console.WriteLine("Enter Your Name: ");
+            //string Name = Console.ReadLine();
+
+            //Console.WriteLine("Enter Your Age: ");
+            //int Age = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter Your GPA: ");
+
+            //double GPA = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Name : " + Name);
+            //Console.WriteLine("Age : " + Age);
+            //Console.WriteLine("GPA : " + GPA);
+
+            //int Age = int.Parse(Console.ReadLine());
+            //int Age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine( Age);
+
+            //int Age;
+            //int.TryParse(s: Console.ReadLine(), out Age);
+            //Console.WriteLine(Flag);
+            //Console.WriteLine(Age);
+
+            //Console.WriteLine("Hello After Error"); 
+            #endregion
+
+            #region Fractions And Discards
+            // Fractions 
+            //2.5 ---> Double
+            //12 --> int
+            //double x = 12.5;
+            //float y = 1.5f;
+            //decimal z = 2.5m;
+
+            //Discards
+            //long X = 100_000_000_000;
+            //Console.WriteLine(X); 
+            #endregion
+
+            #region Operators
+
+            //++
+            //int x = 10;
+            //Console.WriteLine(++x);// 11
+            //////////
+            //Console.WriteLine(x++);// 10
+            //Console.WriteLine(x);//11
+
+
+            //--
+            //int x = 10;
+            //Console.WriteLine(--x);// 9
+            //////////
+            //Console.WriteLine(x--);// 10
+            //Console.WriteLine(x);//9
+
+            //==================================
+            //2.Binary Operators
+            //+ = / * %
+
+            //int sum, mul, sub, mod
+            //int Number01 = 2, Number02 = 6;
+            //sum = Number01 + Number02;//8
+            //mul = Number01 * Number02;//12
+            //sub = Number01 - Number02;//-4
+            //mod = mul % mul;//2
+            //23%3=2
+            //==================================
+            //4.Relational Operators
+            //int x = 6, y = 5;
+            //Console.WriteLine(x==y);
+            //Console.WriteLine(x != y);
+            //Console.WriteLine(x > y);
+            //Console.WriteLine(x < y);
+            //Console.WriteLine(x >= y);
+            //Console.WriteLine(x <= y);
+
+            //5. Logical Operators
+            //Console.WriteLine(!false);// ! -> Not
+            //Console.WriteLine(false && true);// && -> AND short Circuit
+            //Console.WriteLine(false || true);// || -> OR short Circuit
+
+            //6. Bitwise Operators
+            //Console.WriteLine(!false);// ! -> Not
+            //Console.WriteLine(false & true);// && -> AND Long Circuit
+            //Console.WriteLine(false | true);// || -> OR Long Circuit
+
+            //7. Ternary Operators
+            //bool flag = 4 > 2;
+            //Console.WriteLine(flag);
+            //string flag =4< 2 ? "Correct" : "Not Correct";
+            //Console.WriteLine(flag);
+            #endregion 
+
+            #region Operators Priorty
+
+            //int a = 20;
+            //int b = 10;
+            //int c = 15;
+            //int d = 30;
+            //int e;
+            //e = (a + b) * c / d;
+            //e = ((a + b) * c) / d;
+            //e = (a + b) * (a / d);
+            //e = a + (b * c) / b;
+
+            #endregion
+        }
+    }
+}
